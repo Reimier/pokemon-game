4aa99f52690129fc558343dfeb8dafba4b27cb3e
Rei Seyer <reimierreyes0221@gmail.com>
2025-11-25 15:46:14 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,29 @@
-
-import NavBar from "./components/NavBar/NavBar"
-import {BrowserRouter, Route, Routes} from "react-router"
-import PokemonCardEasy from "./components/PokemonCardEasy/PokemonCardEasy"
-import Contact from "./pages/Contact/Contact"
-import PokemonCardHard from "./components/PokemonCardHard/PokemonCardHard"
-import PokemonCardNormal from "./components/PokemonCardNormal/PokemonCardNormal"
+import NavBar from "./components/NavBar/NavBar";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import PokemonCardEasy from "./components/PokemonCardEasy/PokemonCardEasy";
+import Contact from "./pages/Contact/Contact";
+import PokemonCardHard from "./components/PokemonCardHard/PokemonCardHard";
+import PokemonCardNormal from "./components/PokemonCardNormal/PokemonCardNormal";
 
 function App() {
-  
-
   return (
     <>
+      <Router>
+        <NavBar />
 
-     <BrowserRouter>
-
-      <NavBar/>
-
-      <div id="main-container">
-        <Routes>
-          <Route path="/*" element={<PokemonCardEasy />} />
-          <Route path="/easy" element={<PokemonCardEasy />} />
-          <Route path="/normal" element={<PokemonCardNormal/>} />
-          <Route path="/hard" element={<PokemonCardHard/>} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-
-    </BrowserRouter>
-
-
-     
+        <div id="main-container">
+          <Routes>
+            <Route path="/" element={<PokemonCardEasy />} />
+            <Route path="/easy" element={<PokemonCardEasy />} />
+            <Route path="/normal" element={<PokemonCardNormal />} />
+            <Route path="/hard" element={<PokemonCardHard />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<PokemonCardEasy />} />
+          </Routes>
+        </div>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
